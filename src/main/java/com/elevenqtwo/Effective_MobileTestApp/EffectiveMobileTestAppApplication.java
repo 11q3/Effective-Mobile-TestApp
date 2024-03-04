4f136b6541bc2c459688406c6b597bd2104031a5
eleventqwo <elevenqtwo@gmail.com>
2024-03-05 02:23:44 +0300

    template of project

---
+++ src/main/java/com/elevenqtwo/Effective_MobileTestApp/EffectiveMobileTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.elevenqtwo.Effective_MobileTestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveMobileTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffectiveMobileTestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elevenqtwo/Effective_MobileTestApp/EffectiveMobileTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elevenqtwo.Effective_MobileTestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveMobileTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
